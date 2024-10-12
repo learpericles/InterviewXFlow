3165eb49729ff1ce35191376922db48bf5270ec4
maxim <learpericles@gmail.com>
2024-10-12 14:36:16 +0400

    Refactored Example2_2

--- Example2/Example2_2.cs
+++ Example2/Example2_2.cs
@@ -2,10 +2,13 @@ public class ExtPlayer : Player
 {
 	private Action innerChanged;
 
+	// - Убран вызов делегата в момент подписки, так как это нарушает саму идею делегата Changed.
+	// - Потенциальная проблема с отсутствием передачи данных о том, что изменилось. Подписчикам придется обращаться
+	//	 непосредственно к объекту для получения доп.информации, что в свою очередь, увеличивает связность
+	// - Наименование поля слишком общее, желательно детализировать названием компонента, который изменился
 	public event Action Changed {
 		add {
 			innerChanged += value;
-			value();
 		}
 		remove {
 			innerChanged -= value;
@@ -13,6 +16,7 @@ public class ExtPlayer : Player
 	}
 }
 
+// Не нравится, статическая природа полей и методов класса
 class ExtProgram : Program
 {
 	// Виджет, отображающий игроку здоровье.
@@ -31,9 +35,12 @@ class ExtProgram : Program
 		HitPlayer();
 	}
 
+	// Аналогичные вопросы как и к Example2_1
 	private static void OnPlayerChanged()
 	{
 		healthView.Text = player.Health.ToString();
+
+		// Видно, что View не покраснеет при первом изменении здоровья
 		if (previousHealth != null && player.Health - previousHealth < -10) {
 			healthView.Color = Color.Red;
 		} else {
