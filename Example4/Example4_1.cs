ab538a6064ae5c371552f687b6d8bd08fb9dcb18
maxim <learpericles@gmail.com>
2024-10-13 13:13:23 +0400

    Commented Example4_1

--- Example4/Example4_1.cs
+++ Example4/Example4_1.cs
@@ -15,6 +15,17 @@ public class CheatElementBehaviour : MonoBehaviour
     }
 }
 
+// Менеджер нарушает DIP и SRP принципы, здесь как управление жизненным циклом UI, так и хранение провайдеров читов,
+// плюс сам по себе синглтон нарушает SRP.
+//
+// Желательно,извлечь интерфейс ICheatManager, который бы работал непосредственно с провайдерами.
+// Для работы с UI можно создать интерфейс ICheatPanelController, который бы управлял жизненным циклом UI
+//
+// Также желательно, избавиться от синглтона, чтобы иметь возможность внедрять менеджер как зависимость
+//
+// Не нравится использование вложенных классов и интерфейсов
+//
+// Желательно, всю работы с читами держать в отдельной сборке, чтобы иметь возможность не добавлять ее для прод билдов
 public class CheatManager
 {
     public class CheatActionDescription
@@ -81,15 +92,23 @@ public class CheatManager
     }
 }
 
+// Нарушается SRP принцип. Здесь как предоставление списка читов, так и работа с состоянием поля _health
+// Неясный нейминг класса
 public class SomeManagerWithCheats : CheatManager.ICheatProvider
 {
     private int _health;
 
+	// Прямая зависимость от реализации менеджера
+	// Сам по себе менеджер читов уже имеет метод для регистрации провайдера, лучше перенести логику регистрации
+	// провайдера в менеджер - это позволит уменьшить связность
+	// В рамках примера, метод нигде не вызывается
     public void Setup()
     {
         CheatManager.Instance.RegProvider(this);
     }
 
+	// Методы c yield на уровне IL выглядят как отдельный класс с машиной состояния, чтобы снизить издержки, в рамках
+	// данного примера, можно просто возвращать коллекцию читов
     IEnumerable<CheatManager.CheatActionDescription> CheatManager.ICheatProvider.GetCheatActions()
     {
         yield return new CheatManager.CheatActionDescription("Cheat health", () => _health++);
